42ea5e88371b0c3f3ffac0e9d3239453899bd436
Jared Dahlke <jared.dahlke@inxeption.com>
2022-12-08 09:58:56 -0800

    adds holidays through 2026

--- index.js
+++ index.js
@@ -3,30 +3,52 @@ const app = express()
 const port = 3005
 var moment = require('moment-business-days') //https://github.com/kalmecak/moment-business-days
 
-const christmas2022 = '2022-12-26'
-const newYears2023 = '2023-01-02'
-const mlkDay2023 = '2023-01-16'
-const washingtonsBday2023 = '2023-02-20'
-const memorialDay2023 = '2023-05-29'
-const juneTeenth2023 = '2023-06-19'
-const independenceDay2023 = '2023-07-04'
-const laborDay2023 = '2023-09-04'
-const veteransDay2023 = '2023-11-10'
-const thanksgiving2023 = '2023-11-23'
-const christmasDay2023 = '2023-12-25'
-
 const holidaysArr = [
-	christmas2022,
-	newYears2023,
-	mlkDay2023,
-	washingtonsBday2023,
-	memorialDay2023,
-	juneTeenth2023,
-	independenceDay2023,
-	laborDay2023,
-	veteransDay2023,
-	thanksgiving2023,
-	christmasDay2023
+	'2022-12-26',
+	'2023-01-02',
+	'2023-01-16',
+	'2023-02-20',
+	'2023-05-29',
+	'2023-06-19',
+	'2023-07-04',
+	'2023-09-04',
+	'2023-10-09',
+	'2023-11-10',
+	'2023-11-23',
+	'2023-12-25',
+	'2024-01-01',
+	'2024-01-15',
+	'2024-02-19',
+	'2024-05-27',
+	'2024-06-19',
+	'2024-07-04',
+	'2024-09-02',
+	'2024-10-14',
+	'2024-11-11',
+	'2024-11-28',
+	'2024-12-25',
+	'2025-01-01',
+	'2025-01-20',
+	'2025-02-17',
+	'2025-05-26',
+	'2025-06-19',
+	'2025-07-04',
+	'2025-09-01',
+	'2025-10-13',
+	'2025-11-11',
+	'2025-11-27',
+	'2025-12-25',
+	'2026-01-01',
+	'2026-01-19',
+	'2026-02-16',
+	'2026-05-25',
+	'2026-06-19',
+	'2026-07-03',
+	'2026-09-07',
+	'2026-10-12',
+	'2026-11-11',
+	'2026-11-26',
+	'2026-12-25'
 ]
 
 moment.updateLocale('us', {
@@ -73,7 +95,7 @@ const getPaymentDatesWithLateDate = (deliveryDate, installmentsCount) => {
 }
 
 app.listen(port, () => {
-	const deliveryDate = '2023-01-31'
+	const deliveryDate = '2023-03-06'
 	const installmentsCount = 20
 	const dates = getPaymentDatesWithLateDate(deliveryDate, installmentsCount)
 	console.log('deliveryDate: ', deliveryDate)
